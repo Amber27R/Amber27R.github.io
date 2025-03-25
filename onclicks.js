64008b79f658fc16cfe3e842023a1aa28a707ee7
Amber27R <seanambtcs@gmail.com>
2025-03-25 22:00:21 +0000

    Add files via upload

---
+++ onclicks.js
@@ -0,0 +1,16 @@
+function ActionTV() {
+  document.getElementById("genreOne").innerHTML = "The falcon and The Winter Soldier, The Punisher, Invincible, Arcane";
+}
+
+function ThrillerTV() {
+  document.getElementById("genreTwo").innerHTML = "Black Mirror, American Horror Story, Severance, Squid Game";
+}
+
+function ScifiTV() {
+  document.getElementById("genreThree").innerHTML = "Stranger Things, Lost, Doctor Who, The Mandolorian";
+}
+
+function FantasyTV() {
+  document.getElementById("genreFour").innerHTML = "Supernatural, Wednesday, House of the Dragon, Lucifer";
+}
+
